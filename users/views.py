aed973ec403888f5e74623e09adc072ba0f2fa64
caio <chss.bolsa99@gmail.com>
2023-06-22 14:33:25 -0300

    feat tarefa 3 finalizada

--- users/views.py
+++ users/views.py
@@ -9,5 +9,9 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
 class UserView(CreateAPIView):
     serializer_class = UserSerializer
-
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
        
