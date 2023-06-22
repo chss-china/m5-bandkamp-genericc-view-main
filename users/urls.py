3f9447a2e4b33ecdd75a45f3ebc7c93f71d32706
caio <chss.bolsa99@gmail.com>
2023-06-22 14:31:35 -0300

    tarefa 2 finalizada

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-    SpectacularRedocView,
 
-)
 
    
 urlpatterns = [
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+   
     path("users/", views.UserView.as_view()),
     path("users/<int:pk>/", views.UserDetailView.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
     
 ]
--- users/views.py
+++ users/views.py
@@ -9,9 +9,5 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
 class UserView(CreateAPIView):
     serializer_class = UserSerializer
-class UserDetailView(RetrieveUpdateDestroyAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAccountOwner]
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+
        
